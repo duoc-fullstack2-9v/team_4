f1bec15dee27b8601d873f001471372650a17c2b
Luis Melendez <l.melendez@duocuc.cl>
2025-12-04 04:40:45 -0300

    integracion de backend en Productos, modificacion de sus componentes e styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,11 +3,16 @@ import Hero from "./Hero"
 import ListaProductos from "./ListaProductos"
 
 function Main(props) {
+
+    // Dividir los productos en grupos de 4
+    const productosAgrupados = [];
+    for (let i = 0; i < props.productos.length; i += 4) {
+        productosAgrupados.push(props.productos.slice(i, i + 4));
+    }
+
     return (<main>
-        {props.showHero && <Hero isLoggedIn={props.isLoggedIn}/>}
-        {props.productos.map((item, index) =>(
-            <ListaProductos key = {index} productos = {item}></ListaProductos>
-        ))}
+        {props.showHero && <Hero isLoggedIn={props.isLoggedIn} />}
+        <ListaProductos productos={props.productos}></ListaProductos>
 
     </main>);
 
